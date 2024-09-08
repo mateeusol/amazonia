88cac4ff0c6d2ab22e0d3b29e4f6ab56608fa256
Mateeus <mateusguimaraes2122@gmail.com>
2024-09-08 19:00:38 -0400

    amazonia

---
+++ dados.js
@@ -0,0 +1,79 @@
+let dados = [
+  {
+    titulo: "Onça Pintada",
+    descricao:
+      "A onça-pintada é um dos felinos mais majestosos e poderosos do continente americano. Conhecida por sua beleza e força, ela é um símbolo da fauna brasileira e de outras regiões da América Latina.",
+    link: "https://oncafari.org/especie_fauna/onca-pintada/",
+    tags: "onca pintada felino gatinho gato"
+  },
+  {
+    titulo: "Arara Azul",
+    descricao:
+      "A arara azul é uma das aves mais impressionantes do Brasil, conhecida por sua plumagem vibrante e comportamento social.",
+    link: "https://brasilescola.uol.com.br/animais/arara-azul.htm",
+    tags: ""
+  },
+  {
+    titulo: "Jacaré-Açu",
+    descricao:
+      "O jacaré-açu é uma das maiores espécies de jacaré, encontrado principalmente na região amazônica.",
+    link: "https://www.biodiversity4all.org/taxa/116532-Melanosuchus-niger",
+    tags: ""
+  },
+  {
+    titulo: "Boto Cor-de-Rosa",
+    descricao:
+      "O boto cor-de-rosa é um golfinho de água doce encontrado na Amazônia, conhecido por sua cor distinta e inteligência.",
+    link: "https://brasilescola.uol.com.br/folclore/boto-cor-de-rosa.htm",
+    tags: ""
+  },
+  {
+    titulo: "Macaco Uacari",
+    descricao:
+      "O macaco uacari é conhecido por seu rosto vermelho e é uma espécie ameaçada encontrada na Amazônia.",
+    link: "https://mamiraua.org.br/jogos/animais-das-rds-amana-e-mamiraua/mamiferos/",
+    tags: ""
+  },
+  {
+    titulo: "Anta",
+    descricao:
+      "A anta é o maior mamífero terrestre da América do Sul, conhecida por seu corpo robusto e comportamento solitário.",
+    link: "https://oncafari.org/especie_fauna/anta/",
+    tags: ""
+  },
+  {
+    titulo: "Tamanduá-Bandeira",
+    descricao:
+      "O tamanduá-bandeira é um mamífero noturno com uma longa língua e é conhecido por sua habilidade em se alimentar de insetos.",
+    link: "https://www.wwf.org.br/natureza_brasileira/especiais/biodiversidade/especie_do_mes/junho_tamandua_bandeira/",
+    tags: ""
+  },
+  {
+    titulo: "Pirarucu",
+    descricao:
+      "O pirarucu é um dos maiores peixes de água doce do mundo, encontrado na região amazônica.",
+    link: "https://www.wwf.org.br/natureza_brasileira/especiais/biodiversidade/especie_do_mes/agosto_pirarucu/",
+    tags: ""
+  },
+  {
+    titulo: "Gavião-Real",
+    descricao:
+      "O gavião-real é uma águia majestosa encontrada nas florestas da América do Sul, conhecida por sua grande envergadura e habilidade de caça.",
+    link: "https://brasilescola.uol.com.br/animais/gaviao.htm",
+    tags: ""
+  },
+  {
+    titulo: "Sucuri",
+    descricao:
+      "A sucuri é uma das maiores serpentes do mundo, encontrada em áreas úmidas da América do Sul.",
+    link: "https://www.biologianet.com/biodiversidade/sucuri.htm",
+    tags: ""
+  },
+  {
+    titulo: "Preguiça",
+    descricao:
+      "A preguiça é um mamífero arborícola de movimentos lentos, conhecido por sua vida tranquila e seu comportamento único.",
+    link: "https://www.biologianet.com/biodiversidade/bicho-preguica.htm",
+    tags: ""
+  },
+];
